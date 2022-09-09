8f7dbfd30867fa9921bb65c9c448bfbca87f5985
Ocomic <108867917+Ocomic@users.noreply.github.com>
2022-09-09 16:40:21 +0200

    1st Test
    First programming test

--- Program.cs
+++ Program.cs
@@ -1,11 +1,11 @@
 ﻿//New format of Console Script
-Console.WriteLine("Hello World!");
+//Console.WriteLine("Hello World!");
 
 
 
 //Old format of Console Script
 
-/*using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,7 +19,14 @@ namespace Calculator
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello I am a program!");
+            Console.WriteLine("Press any key to close the console.");
+
+            // use ReadKey to close console by pressing any key
+            // while (Console.ReadKey().Key != ConsoleKey.E) can be used to close console with E key for example
+            Console.ReadKey();
+            //use Console.ReadLine(); to colse console with enter key
+
 
         }
 
@@ -27,4 +34,4 @@ namespace Calculator
 
 
 
-}*/
+}
