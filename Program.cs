1a8466e08b1aca8188095c6688bf760210e06e04
Ocomic <108867917+Ocomic@users.noreply.github.com>
2022-09-09 23:57:36 +0200

    Added clalcualtion of two numers using a sum

--- Program.cs
+++ Program.cs
@@ -1,11 +1,4 @@
-﻿//New format of Console Script
-//Console.WriteLine("Hello World!");
-
-
-
-//Old format of Console Script
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,21 +6,36 @@ using System.Threading.Tasks;
 
 namespace Calculator
 {
-
+    /// <summary>
+    /// Udemy course: https://www.udemy.com/course/einstieg-in-csharp-software-programmieren-wie-ein-profi/learn/lecture/4126070#overview
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Hello I am a program!");
-            Console.WriteLine("Press any key to close the console.");
+            // User Story: As a user I want to have a function that can do a calculation
+            // first Iteration is to implement addition (+) of two or more numbers (make a SUM)
+            Console.Write("Please enter first number:");
+            string firstNumber = Console.ReadLine();
+            Console.Write("Please enter second number:");
+            string secondNumber = Console.ReadLine();
 
-            // use ReadKey to close console by pressing any key
-            // while (Console.ReadKey().Key != ConsoleKey.E) can be used to close console with E key for example
-            Console.ReadKey();
-            //use Console.ReadLine(); to colse console with enter key
+            // convert string to int for calculation
+            int firstNumberInt = Convert.ToInt32(firstNumber);
+            int secondNumberInt = Convert.ToInt32(secondNumber);
 
+            // calculates sum of two numbers
+            int sum = firstNumberInt + secondNumberInt;
 
+            // return value of calculation
+            Console.WriteLine("The solution is: {0}", sum);
+            Console.ReadLine();
+            
         }
 
     }
